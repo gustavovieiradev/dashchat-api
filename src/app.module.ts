91d54defe60176b43b19497f77040d754eb3e07e
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-05-23 16:19:01 -0300

    refactor(app-module): prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { BancoMongodb } from './shared/enums/banco-mongodb.enum';
 import { AutenticacaoModule } from './modules/autenticacao/autenticacao.module';
 import { TabelaMongodb } from './shared/enums/tabela-mongodb.enum';
 import { UsuarioSchema } from './modules/autenticacao/schemas/usuario.schema';
-import {JwtModule} from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { ClienteModule } from './modules/cliente/cliente.module';
 import { ProjetoModule } from './modules/projeto/projeto.module';
 import * as configJwt from './shared/config-jwt';
@@ -20,22 +20,27 @@ import * as configJwt from './shared/config-jwt';
       useUnifiedTopology: true,
       connectionName: BancoMongodb.DASHCHAT,
     }),
-    MongooseModule.forFeature([
-        { name: TabelaMongodb.Usuario, schema: UsuarioSchema, collection: TabelaMongodb.Usuario },
-    ], BancoMongodb.DASHCHAT),
+    MongooseModule.forFeature(
+      [
+        {
+          name: TabelaMongodb.Usuario,
+          schema: UsuarioSchema,
+          collection: TabelaMongodb.Usuario,
+        },
+      ],
+      BancoMongodb.DASHCHAT,
+    ),
     JwtModule.register({
       secretOrPrivateKey: configJwt.JWT_SECRETO,
       signOptions: {
-          expiresIn: configJwt.EXP_JWT,
+        expiresIn: configJwt.EXP_JWT,
       },
-  }),
+    }),
     AutenticacaoModule,
     ClienteModule,
     ProjetoModule,
   ],
-  controllers: [
-  ],
-  providers: [
-  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
